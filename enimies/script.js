a229bd6cb08e98e4cb6350f62f4c5f2924f9f133
Harsh Wardhan <harshwardhan847@gmail.com>
2023-09-13 15:48:49 +0530

    random movements

--- enimies/script.js
+++ enimies/script.js
@@ -5,36 +5,39 @@ const ctx = canvas.getContext("2d");
 
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 50;
+const numberOfEnemies = 10;
 const enemiesArray = [];
 let gameFrame = 0;
 
 class Enemy {
   constructor() {
     this.enemyImage = new Image();
-    this.enemyImage.src = "./enemiesImg/enemy3.png";
+    this.enemyImage.src = "./enemiesImg/enemy4.png";
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 218;
-    this.spriteHeight = 177;
+    this.spriteWidth = 213;
+    this.spriteHeight = 213;
 
     this.width = this.spriteWidth / 2;
     this.height = this.spriteHeight / 2;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
+    this.newX = Math.random() * (canvas.width - this.width);
+    this.newY = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = Math.random() * 2;
-    this.andleSpeed = Math.random() * 1.5 + 0.5;
-    // this.curve = Math.random() * 200;
+    this.interval = Math.floor(Math.random() * 200 + 50);
   }
   update() {
-    this.x =
-      (canvas.width / 2) * Math.sin((this.angle * Math.PI) / 90) +
-      (canvas.width / 2 - this.width / 2);
-    this.y =
-      (canvas.height / 2) * Math.cos((this.angle * Math.PI) / 180) +
-      (canvas.height / 2 - this.height / 2);
-    this.angle += this.andleSpeed;
+    if (gameFrame % this.interval === 0) {
+      this.newX = Math.random() * (canvas.width - this.width);
+      this.newY = Math.random() * (canvas.height - this.height);
+    }
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    this.x -= dx / 70;
+    this.y -= dy / 70;
+    // this.x =0
+    // this.y =0
     if (this.x + this.width < 0) this.x = canvas.width;
     //animate sprite
     if (gameFrame % this.flapSpeed === 0)
